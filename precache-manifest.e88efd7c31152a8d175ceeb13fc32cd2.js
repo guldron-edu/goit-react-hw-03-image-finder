1897bb2a2950c48a97f6f4f998ecf2b09d78fdd9
guldron-edu <guldron.edu@gmail.com>
2020-09-23 16:39:18 +0300

    Updates

---
+++ precache-manifest.e88efd7c31152a8d175ceeb13fc32cd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d7ec64a8a7c11b1ceb7315fbe8574a",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "5cc15acc88dae534aed2",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "2dfcac87e52c59c15f04",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.b0e59120.chunk.css"
+  },
+  {
+    "revision": "5cc15acc88dae534aed2",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.7b604f78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.7b604f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dfcac87e52c59c15f04",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.d6b3ccad.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  },
+  {
+    "revision": "0a7c4b1bd840e39bef722d107d399770",
+    "url": "/goit-react-hw-03-image-finder/static/media/search.0a7c4b1b.svg"
+  }
+]);
